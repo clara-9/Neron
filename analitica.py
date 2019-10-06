b1c7d4dcdd3b8be71d9612b23aba267e4a0290bb
Nevvton <36523496+Nevvton@users.noreply.github.com>
2019-10-06 06:29:54 +0200

    Add files via upload

--- analitica.py
+++ analitica.py
@@ -20,23 +20,28 @@ RBC=[4300000,5900000]
 Hmo=[12.5,17]
 VCM=[70,100]
 Glu=[70,110]
-Ure=[0.6,1.5]
 Cre=[70,110]
+Ure=[0.6,1.5]
 LDL=[0,130]
-
-rest=[]
+Leu=[4000,12000]
+Cet=[0,5]
+Nit=[0,0.05]
+Pro=[0,20]
+Leu=[30,40]
+Bac=[0,5]
+Sat=[95,100]
+RPM=[60,170]
 
 now=datetime.datetime.now()
 
-categories=['Hem','Bio']
+categories=['Hem','Bio','Ori','Car']
 
 
 
 
+pacients=[]
 
-
-
-for k in range(1,3):
+for k in range(1,7):
     file=open('analitica%s.txt'%k,'r')
     
     info=file.readlines()
@@ -46,6 +51,7 @@ for k in range(1,3):
     
     for cat in categories:
         exec('%s_%s={}'%(cat,pacient))
+    pacients.append(pacient)
         
 
     persona=info[0][0:(len(info[0])-1)]
@@ -64,19 +70,57 @@ for k in range(1,3):
         
         
 
-#Leu=[4000,12000]
 
 
 
 
-    for test in categories:
-        for j in eval('%s_%s'%(test,pacient)):
-            res=estimacio(eval(j),eval('%s_%s'%(test,pacient))['%s'%j])
-            if res<=np.exp(1):
-                res=0
+    rest=[0]
+    for cat in categories:
+        for test in eval('%s_%s'%(cat,pacient)):
+            res=estimacio(eval(test),eval('%s_%s'%(cat,pacient))['%s'%test])
+            if res<=(np.exp(1)+0.0001):
+                res=0.0
             else:
                 res=res-np.exp(1)
+            if res>max(rest):
+                testmax=test
             rest.append(res)
+    totaldolents=0
+    for i in rest:
+        if i>0:
+            totaldolents+=1
+    eval(pacient)['suma']=sum(rest)
+    eval(pacient)['pitjor']=testmax
+    eval(pacient)['valorpitjor']=max(rest)
+    eval(pacient)['testdolents']=totaldolents
+    eval(pacient)['testtotals']=totaldolents/len(rest)
+
+def prioritat(pacient):
+    percent=pacient['testtotals']
+    suma=pacient['suma']
+    if percent>0.8 and suma>20:
+        prioritat='ALTA'
+    elif percent>50:
+        if suma<5:
+            prioritat='BAIX'
+        elif suma<15:
+            prioritat='MIG'
+        else:
+            prioritat='ALT'
+    elif percent<20:
+        if suma<10:
+            prioritat='BAIX'
+        elif suma<25:
+            prioritat='MIG'
+        else:
+            prioritat='ALT'
+    else:
+        prioritat='BAIX'
+    return prioritat
+        
+for i in pacients:
+    eval(i)['prioritat']=prioritat(eval(i))
+            
             
     
 #eval(i)['%s'%j],j,eval(j)
